8d1d4a509fe3e80c5a101b752b11e6aefd1a48c1
meriaizen86 <trollainas@gmail.com>
2022-07-17 21:04:44 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.GameDefaults.ProceduralModels;
+using Stride.GameDefaults.Extensions;
+
+using (var game = new Game())
+{
+    game.Run(start: Start);
+
+    void Start(Scene rootScene)
+    {
+        game.SetupBase3DScene();
+
+        var entity = game.CreatePrimitive(PrimitiveModelType.Capsule);
+
+        entity.Transform.Position = new Vector3(0, 9, 0);
+        entity.Scene = rootScene;
+    }
+}
